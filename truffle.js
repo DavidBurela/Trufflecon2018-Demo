c541850ed3e904a38d535f11becd5eb9f73fae4d
David Burela <David.Burela@gmail.com>
2018-10-06 11:38:35 -0700

    added junit output

--- truffle.js
+++ truffle.js
@@ -1,4 +1,12 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+
+  // truffle.js - specify mocha output options
+mocha: {
+  reporter: "mocha-junit-reporter",
+  reporterOptions: {
+    mochaFile: 'TEST-truffle.xml'
+  }
+}
 };
